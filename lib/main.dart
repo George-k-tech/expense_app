7644cc7f0367717dc1d718d7aee0f7a49126edd2
George-k-tech <kariukigeorge511@gmail.com>
2023-02-02 17:19:48 +0300

    updates main and transaction page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,12 +6,54 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'new shoes',
+      amount: 69.08,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'new clothes',
+      amount: 169.08,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('expense application'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('chart!'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
